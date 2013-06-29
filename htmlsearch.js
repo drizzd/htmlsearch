d4d1e969f554ccf0827b201c957ed1c42d2da2ea
Clemens Buchacher <drizzd@aon.at>
2013-06-29 14:40:26 +0200

    optional backward search

--- htmlsearch.js
+++ htmlsearch.js
@@ -57,9 +57,16 @@ function nodeAt(nodeArray, index) {
 	return null;
 }
 
-function indexAt(textNodes, range) {
-	var node = range.endContainer;
-	var offset = range.endOffset;
+function indexAt(textNodes, range, forward) {
+	var node, offset;
+
+	if (forward) {
+		node = range.endContainer;
+		offset = range.endOffset;
+	} else {
+		node = range.startContainer;
+		offset = range.startOffset - 1;
+	}
 
 	var index = textNodes.indexOf(node);
 	if (index < 0) {
@@ -69,28 +76,53 @@ function indexAt(textNodes, range) {
 	return visibleText(textNodes.slice(0, index)).length + offset;
 }
 
-function search(pattern, startAt) {
+function find_string_1(text, pattern, forward, startOffset) {
+	if (forward) {
+		return text.indexOf(pattern, startOffset);
+	} else {
+		if (startOffset < 0) {
+			return -1;
+		}
+		return text.lastIndexOf(pattern, startOffset);
+	}
+}
+
+function find_string(text, pattern, forward, startOffset) {
+	var wraparound = false;
+	var index = find_string_1(text, pattern, forward, startOffset);
+	if (index < 0) {
+		wraparound = true;
+		startOffset = 0;
+		if (!forward) {
+			startOffset = text.length;
+		}
+		var index = find_string_1(text, pattern, forward, startOffset);
+	}
+
+	return { "index": index, "wraparound": wraparound };
+}
+
+function search(pattern, forward, startAt) {
+	if (forward == null) {
+		forward = true;
+	}
 	var textNodes = visibleTextNodes(document.body);
 	var startOffset = 0;
 	if (startAt != null) {
-		startOffset = indexAt(textNodes, startAt);
+		startOffset = indexAt(textNodes, startAt, forward);
 	}
 
-	var wraparound = false;
 	var text = visibleText(textNodes);
-	var index = text.indexOf(pattern, startOffset);
-	if (index < 0) {
-		var index = text.indexOf(pattern);
-		if (index < 0)
-			return null;
-		wraparound = true;
+	var match = find_string(text, pattern, forward, startOffset);
+	if (match.index < 0) {
+		return null;
 	}
 
 	var range = document.createRange();
-	range.setStart.apply(range, nodeAt(textNodes, index));
-	range.setEnd.apply(range, nodeAt(textNodes, index + pattern.length));
+	range.setStart.apply(range, nodeAt(textNodes, match.index));
+	range.setEnd.apply(range, nodeAt(textNodes, match.index + pattern.length));
 
-	return { "range": range, "wraparound": wraparound };
+	return { "range": range, "wraparound": match.wraparound };
 }
 
 function select(range) {
@@ -98,14 +130,14 @@ function select(range) {
 	window.getSelection().addRange(range);
 }
 
-function select_match(pattern) {
+function select_match(pattern, forward) {
 	var selection = window.getSelection();
 	var startAt = null;
 	if (selection.type != "None") {
 		startAt = selection.getRangeAt(0);
 	}
 
-	var match = search(pattern, startAt);
+	var match = search(pattern, forward, startAt);
 	if (match != null) {
 		select(match.range)
 	}
